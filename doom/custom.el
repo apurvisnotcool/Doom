fba95746678c8523a640f5039b98b6fc596b54bd
Apurva Poddar <113971943+apurvisnotcool@users.noreply.github.com>
2023-10-04 23:19:44 +0530

    Add files via upload

---
+++ doom/custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("c7f0d3df4899a053e0ff40e88bc70e6a3c0b014b26a192fac4ccbf6c85e87b04" "c335adbb7d7cb79bc34de77a16e12d28e6b927115b992bccc109fb752a365c72" "7c7026a406042e060bce2b56c77d715c3a4e608c31579d336cb825b09e60e827" "fa7caecc85dd0aaf60d4f74e42300a1a69f32efbad61fbd3ca26d0dcf6dfedd5" "60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" "4363ac3323e57147141341a629a19f1398ea4c0b25c79a6661f20ffc44fdd2cb" default))
+ '(ede-project-directories '("/home/gamma/Game_of_life/Assets"))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(package-selected-packages
+   '(afternoon-theme nordic-night-theme dotnet mono-complete vcsh)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(bold ((t (:weight bold))))
+ '(epa-string ((t (:inherit nil :background "chartreuse"))))
+ '(font-lock-string-face ((t (:foreground "chartreuse"))))
+ '(marginalia-string ((t (:inherit nil :background "chartreuse"))))
+ '(region ((t (:inherit nil :extend t :background "dim gray")))))
+(put 'customize-themes 'disabled nil)
+(put 'customize-face 'disabled nil)
