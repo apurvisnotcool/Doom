b93f151a14ab199e6d8fd0387aaecfff8133cb67
Apurva Poddar <113971943+apurvisnotcool@users.noreply.github.com>
2024-03-03 05:45:42 +0530

    Update custom.el

--- doom/custom.el
+++ doom/custom.el
@@ -1,23 +1,31 @@
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#1a1b26" :foreground "#a9b1d6" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight bold :height 113 :width normal :foundry "JB" :family "JetBrains Mono"))))
+ '(bold ((t (:weight bold))))
+ '(epa-mark ((t (:foreground "cyan" :weight bold))))
+ '(epa-string ((t (:inherit nil :background "chartreuse"))))
+ '(font-lock-comment-face ((t (:foreground "white smoke"))))
+ '(font-lock-constant-face ((t (:foreground "green yellow" :weight bold))))
+ '(font-lock-doc-face ((t (:inherit font-lock-comment-face :foreground "cyan"))))
+ '(font-lock-keyword-face ((t (:foreground "cyan" :weight bold))))
+ '(font-lock-string-face ((t (:foreground "chartreuse" :weight bold))))
+ '(hl-line ((t (:extend t :background "#13141c" :weight bold))))
+ '(marginalia-string ((t (:inherit nil :background "chartreuse"))))
+ '(org-code ((t (:inherit org-block :foreground "sandy brown"))))
+ '(region ((t (:inherit nil :background "dim gray")))))
+(put 'customize-themes 'disabled nil)
+(put 'customize-face 'disabled nil)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("c7f0d3df4899a053e0ff40e88bc70e6a3c0b014b26a192fac4ccbf6c85e87b04" "c335adbb7d7cb79bc34de77a16e12d28e6b927115b992bccc109fb752a365c72" "7c7026a406042e060bce2b56c77d715c3a4e608c31579d336cb825b09e60e827" "fa7caecc85dd0aaf60d4f74e42300a1a69f32efbad61fbd3ca26d0dcf6dfedd5" "60ada0ff6b91687f1a04cc17ad04119e59a7542644c7c59fc135909499400ab8" "4363ac3323e57147141341a629a19f1398ea4c0b25c79a6661f20ffc44fdd2cb" default))
- '(ede-project-directories '("/home/gamma/Game_of_life/Assets"))
+   '("4ade6b630ba8cbab10703b27fd05bb43aaf8a3e5ba8c2dc1ea4a2de5f8d45882" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages
-   '(afternoon-theme nordic-night-theme dotnet mono-complete vcsh)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(bold ((t (:weight bold))))
- '(epa-string ((t (:inherit nil :background "chartreuse"))))
- '(font-lock-string-face ((t (:foreground "chartreuse"))))
- '(marginalia-string ((t (:inherit nil :background "chartreuse"))))
- '(region ((t (:inherit nil :extend t :background "dim gray")))))
-(put 'customize-themes 'disabled nil)
-(put 'customize-face 'disabled nil)
+   '(sublime-themes night-owl-theme ggtags company-c-headers ccls @)))
+;;tokyo-night
